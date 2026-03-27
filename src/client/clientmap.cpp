764fa9abe1b06fba96fdaf55539887e92998dcb9
proller <proller@github.com>
2026-03-27 03:51:31 +0100

    better farmesh_all_changed

--- src/client/clientmap.cpp
+++ src/client/clientmap.cpp
@@ -1960,7 +1960,7 @@ void ClientMap::renderMapShadows(video::IVideoDriver *driver,
 
 #if FARMESH_SHADOWS
 		if (!mapBlockMesh) {
-			mapBlockMesh = block->getFarMesh(getFarStep(m_control, getNodeBlockPos(far_blocks_last_cam_pos), block_pos ));
+			mapBlockMesh = block->getFarMesh(farmesh::getFarStep(m_control, getNodeBlockPos(far_blocks_last_cam_pos), block_pos ));
 		}
 #endif
 
@@ -2176,7 +2176,7 @@ void ClientMap::updateTransparentMeshBuffers()
 
 #if FARMESH_SHADOWS
 		if (!blockmesh) {
-			blockmesh = block->getFarMesh(getFarStep(m_control, getNodeBlockPos(far_blocks_last_cam_pos), block->getPos()));
+			blockmesh = block->getFarMesh(farmesh::getFarStep(m_control, getNodeBlockPos(far_blocks_last_cam_pos), block->getPos()));
 		}
 #endif
 
